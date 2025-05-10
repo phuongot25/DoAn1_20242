7f6dc335a7512dd12323a89d917c38869bd302c1
phuongot25 <162326865+phuongot25@users.noreply.github.com>
2025-05-10 11:25:22 +0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace RestaurantManagement
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using RestaurantManagement.Views;
+
+namespace RestaurantManagement
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Giao diện đầu tiên là LoginView
+            this.Content = new LoginView();
+        }
+    }
+}
+
